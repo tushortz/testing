4df0a79c4164ca3114fba7fb575eb319fb97e0f0
Taiwo Kareem <13158672+tushortz@users.noreply.github.com>
2023-11-08 08:41:15 +0000

    Update test.py

--- test.py
+++ test.py
@@ -13,6 +13,4 @@ eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4
 
 secret string == N1PCdw3M2B1TfJhoaY2mL736p2vCUc47
 
-pk_testfjggkgkjrhrhreereyryr464647e
-
-not sure
+sk_testfjggkgkjrhrhreereyryr464647e
