1aa0e317ff5076e7dae5dd876006c38bf17726c2
Taiwo Kareem <taiwo.kareem36@gmail.com>
2024-02-23 01:39:44 +0000

    update

---
+++ test.js
@@ -0,0 +1,15 @@
+
+var s = "surprise!
+    < script > alert('whoops!')
+
+
+window.__INITIAL_STATE__ = <% - JSON.stringify(initialState) %>;
+
+
+analytics.identify(
+    '<%= user.id %>',
+    '<%= request.HTTP_REFERER %>',
+    // ...
+);
+
+  </script >
